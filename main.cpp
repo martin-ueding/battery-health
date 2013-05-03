ccd00231a674328a843f6e5e887ae4fa94e2f6bd
Martin Ueding <dev@martin-ueding.de>
2013-05-03 14:50:16 +0200

    Implement some part

---
+++ main.cpp
@@ -0,0 +1,43 @@
+// Copyright © 2013 Martin Ueding <dev@martin-ueding.de>
+
+#include <iostream>
+#include <fstream>
+
+int main() {
+	int energy_now;
+	int energy_full;
+	int energy_full_design;
+	int power_now;
+
+	double charge;
+	double max_charge;
+	double time_left;
+
+	std::ifstream in;
+
+	in.open("/sys/class/power_supply/BAT0/energy_now");
+	in >> energy_now;
+	in.close();
+
+	in.open("/sys/class/power_supply/BAT0/energy_full");
+	in >> energy_full;
+	in.close();
+
+	in.open("/sys/class/power_supply/BAT0/energy_full_design");
+	in >> energy_full_design;
+	in.close();
+
+	in.open("/sys/class/power_supply/BAT0/power_now");
+	in >> power_now;
+	in.close();
+
+	charge = (double) energy_now / energy_full;
+	max_charge = (double) energy_full / energy_full_design;
+	time_left = (double) energy_now / power_now;
+
+	std::cout << "Current Charge	" << charge << std::endl;
+	std::cout << "Possible Charge	" << max_charge << std::endl;
+	std::cout << "Time Left		" << time_left << std::endl;
+
+	return 0;
+}
