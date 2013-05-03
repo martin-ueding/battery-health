186bb2f5222a070e6f47415af4f628950e8620a2
Martin Ueding <dev@martin-ueding.de>
2013-05-03 15:29:59 +0200

    Print units

--- main.cpp
+++ main.cpp
@@ -38,8 +38,8 @@ int main() {
 	std::cout << "Time		" << time << std::endl;
 	std::cout << "Current Charge	" << charge << std::endl;
 	std::cout << "Possible Charge	" << max_charge << std::endl;
-	std::cout << "Time Left	" << time_left << std::endl;
-	std::cout << "Power Now	" << power_now << std::endl;
+	std::cout << "Time Left	" << time_left << " h" << std::endl;
+	std::cout << "Power Now	" << power_now / 1000000. << " W" << std::endl;
 	std::cout << "Time Until	" << time_until << std::endl;
 
 	return 0;
